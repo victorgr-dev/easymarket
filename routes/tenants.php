b6d2d920388a2109aeaa3fb191916943f6c0d9d4
VGarcia06 <vgarciar06@gmail.com>
2020-07-29 18:45:48 -0500

    .

---
+++ routes/tenants.php
@@ -0,0 +1,13 @@
+<?php
+
+// use Illuminate\Support\Route;
+
+// Route::middleware(['web'])
+//     ->namespace('App\Http\Controllers')
+//     ->as('tenant.')
+//     ->group(function(){
+//         // Route::get('/home', 'HomeController@index')->name('home');
+//         // Route::get('/products', function () {
+//         //    dd(\App\Models\Tenant\Product::all());
+//         // });
+//     });
